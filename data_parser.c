c7d1488f476999e8e338f50c7b5d574e1a589977
Xidruk <xidruk@gmail.com>
2025-01-23 06:19:07 +0100

    Update data_parser.c

--- data_parser.c
+++ data_parser.c
@@ -1,1 +1,22 @@
 #include "g_header.h"
+
+int is_number(char c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    return (0);
+}
+
+int validate_number(char *data)
+{
+    int _index;
+
+    _index = 0;
+    while (data[_index])
+    {
+        if (!is_number(data[_index]))
+            is_not_a_number();
+        _index++;
+    }
+    return (1);
+}
