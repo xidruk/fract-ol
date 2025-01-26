823c4d01826b429429d72f6a34af831b00f99207
Xidruk <xidruk@gmail.com>
2025-01-26 06:36:42 +0100

    Add files via upload

---
+++ debugger/types_swaper_null_param.c
@@ -0,0 +1,8 @@
+#include "debugger.h"
+
+
+void types_swaper_null_param(void)
+{
+    deb_printer("The Given Data To Swap His Types Is NUll ! ...\nThe Process Killed", PERROR_TO_OPENED_TTY);
+    exit(BADESTFLAG);
+}
