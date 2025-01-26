968942e164c64bd573919905eea1f0089007af9f
Xidruk <xidruk@gmail.com>
2025-01-26 02:15:12 +0100

    Update types_swapper.c

--- types_swapper/types_swapper.c
+++ types_swapper/types_swapper.c
@@ -1,7 +1,14 @@
 #include "types_swapper.h"
 
 
-int types_swaper(char *data, int flag)
+void types_swaper(char *data, int flag)
 {
-    
+    if (!data)
+        ptorrent_deb((int)TYPES_SWAPER_NULL_P);
+    if (flag == (int)SWAP_TO_INT)
+        c_to_int(data);
+    else if (flag == (int)SWAP_TO_FLOAT)
+        c_to_float(data);
+    else 
+        ptorrent_deb((int)UNKNOWN_FLAG);
 }
