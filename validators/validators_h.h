640f93d6a974d67dd6266ca4eaf8d56a0651fb15
Xidruk <xidruk@gmail.com>
2025-01-27 15:09:24 +0100

    Create validators_h.h

---
+++ validators/validators_h.h
@@ -0,0 +1,11 @@
+#ifndef VALIDATORS_H
+#define VALIDATORS_H
+
+//==> includes 
+#include "../g_header.h"
+#include "../data_parsing/data_parsing.h"
+
+// validators funcs 
+int julia_validator(int argc, char **argv);
+
+#endif
