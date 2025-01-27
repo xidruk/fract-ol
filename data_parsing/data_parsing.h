b8fd9d3cc9e7b7c12af6f9bc1b7903f2e4301960
Xidruk <xidruk@gmail.com>
2025-01-27 19:34:55 +0100

    Create data_parsing.h

---
+++ data_parsing/data_parsing.h
@@ -0,0 +1,19 @@
+#ifndef DATA_PARSING
+#define DATA_PARSING
+
+// includes 
+
+#include "../pflow_controller/debugger.h"
+
+// data parsing functions 
+
+int is_digit(char c);
+int is_decimal_point(char c);
+int is_minus_sign(char c);
+int is_number(char c);
+int has_valid_minus_sign(char *data);
+int validate_is_float(char *data);
+int check_the_stupid_bug(char *data);
+int validate_number(char *data);
+
+#endif
