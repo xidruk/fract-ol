49ea7998787a741fb86eee23b197f7d1ae1b5f3b
Xidruk <xidruk@gmail.com>
2025-01-27 19:30:52 +0100

    Delete data_parsing directory

--- data_parsing/has_valid_minus_sign.c
+++
@@ -1,6 +0,0 @@
-#include "data_parsing.h"
-
-int has_valid_minus_sign(char *data)
-{
-    return (data[0] == '-' && data[1] != '\0');
-}
--- data_parsing/is_digit.c
+++
@@ -1,6 +0,0 @@
-#include "data_parsing.h"
-
-int is_digit(char c)
-{
-    return (c >= '0' && c <= '9');
-}
