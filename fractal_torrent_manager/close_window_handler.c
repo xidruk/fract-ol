052bd0a25d7a6a1957e87879ed2ac3b2afe16418
Xidruk <xidruk@gmail.com>
2025-02-09 06:46:20 +0100

    Delete fractal_torrent_manager directory

--- fractal_torrent_manager/close_window_handler.c
+++
@@ -1,9 +0,0 @@
-#include "fractal_tm.h"
-
-int handle_close(t_fractal_wc *data)
-{
-    mlx_destroy_window(data->mlx, data->win);
-    mlx_destroy_display(data->mlx);
-    destroy_window_with_close_button();
-    return (0);
-}
